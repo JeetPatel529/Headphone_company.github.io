e8e93cc722772c9793b29ec400bc096d641042f2
Jeet_Patel <kasundrajeet529@gmail.com>
2023-05-21 10:42:53 +0530

    add three new folder and img folder add img index.html file add code

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper("#NewHeadphone", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    navigation: {
+        nextEl: ".rightside_arrow",
+        prevEl: ".leftside_arrow",
+    },
+});
+
+
+var swiper = new Swiper("#Blog", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    navigation: {
+        nextEl: ".rightside_arrow_blog",
+        prevEl: ".leftside_arrow_blog",
+    },
+});
+
+
+
+var swiper = new Swiper("#review", {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    loop: false,
+});
